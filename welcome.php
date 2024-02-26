b81a38444fa7fe10f220e7fb49ae6b6ac6d723c9
Jackson-Davey <159150218+Jackson-Davey@users.noreply.github.com>
2024-02-26 18:54:25 +0000

    added welcome page

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-ca">
+    <head>
+        <title>Welcome page</title>
+    </head>
+    <body>
+        Welcome <?php echo $_POST["name"]; ?><br>
+        Your email address is: <?php echo $_POST["email"]; ?>
+    </body>
+</html>
